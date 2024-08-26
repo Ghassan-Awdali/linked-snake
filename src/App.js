bee5ddc0d7a8c839cfc29c3962bbf28701ccb7e7
G ARUTO <awdalig@gmail.com>
2024-08-26 20:38:47 -0300

    project structure setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Boards from "./Board/Boards.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Boards></Boards>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Board/Boards.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Boards = () => {
+  return <div>Canvas</div>;
+};
+
+export default Boards;
