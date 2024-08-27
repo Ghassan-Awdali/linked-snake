b0d81b17213fce5470e63c7a48d3201a7667abdd
G ARUTO <awdalig@gmail.com>
2024-08-26 21:30:36 -0300

    created cell grid with snake showing on grid, going to make the snake a linked list

--- src/Board/Boards.jsx
+++ src/Board/Boards.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Boards.css";
+
+const BOARD_SIZE = 10;
+
+class LinkedListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor(value) {
+    const node = new LinkedListNode(value);
+    this.head = node;
+    this.tail = node;
+  }
+}
 
 const Boards = () => {
-  return <div>Canvas</div>;
+  const createBoard = (BOARD_SIZE) => {
+    let counter = 1;
+    const board = [];
+    for (let row = 0; row < BOARD_SIZE; row++) {
+      const currentRow = [];
+      for (let col = 0; col < BOARD_SIZE; col++) {
+        currentRow.push(counter++);
+      }
+      board.push(currentRow);
+    }
+
+    return board;
+  };
+
+  const [board, setBoard] = useState(createBoard(BOARD_SIZE));
+  const [snakeCells, setSnakeCells] = useState(new Set([44]));
+  const [snake, setSnake] = useState(new SinglyLinkedList(44));
+
+  return (
+    <div className="board">
+      {board.map((row, rowIdx) => (
+        <div key={rowIdx} className="row">
+          {row.map((cellValue, cellIdx) => (
+            <div
+              key={cellIdx}
+              className={`cell ${
+                snakeCells.has(cellValue) ? "cell-green" : ""
+              }`}
+            >
+              {cellValue}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Boards;
